8b878b376a59b99c3355460c5619dc15b28dc536
petehod <pete@secretlab.com>
2024-08-13 07:38:38 -0500

    restore needed slug file ; remove starter article

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,54 @@
+import { MDXProvider } from "@mdx-js/react";
+import { MDXRemote } from "next-mdx-remote";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { MdxLayout } from "@components/Layout";
+import { BlogPost } from "types/blog.types";
+import { getPostBySlug, getAllPosts } from "@lib/posts";
+import { ArticleHeader } from "@components/Article";
+import { useMDXComponents } from "@components/mdx-components";
+
+interface PostProps {
+  source: any;
+  frontMatter: BlogPost;
+}
+
+const Post: React.FC<PostProps> = ({ source, frontMatter }) => {
+  const components = useMDXComponents({});
+
+  return (
+    <MDXProvider components={components}>
+      <MdxLayout>
+        <ArticleHeader
+          title={frontMatter.title}
+          date={frontMatter.date}
+          image={frontMatter.coverImage}
+        />
+
+        <MDXRemote {...source} />
+      </MdxLayout>
+    </MDXProvider>
+  );
+};
+
+export default Post;
+
+// Implement GetStaticPaths and GetStaticProps for fetching blog data...
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = getAllPosts();
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const post = await getPostBySlug(params?.slug as string);
+
+  return {
+    props: {
+      source: post.mdxSource,
+      frontMatter: post.frontMatter,
+    },
+  };
+};
