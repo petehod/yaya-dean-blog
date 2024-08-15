65ec45bebb38c5ead072854f61fe10fbc2facf46
petehod <pete@secretlab.com>
2024-08-15 06:37:13 -0500

    write more article

--- src/components/Column/ColumnContainer.tsx
+++ src/components/Column/ColumnContainer.tsx
@@ -1,6 +1,6 @@
 export const ColumnContainer = ({ items }: { items: React.ReactNode[] }) => {
   return (
-    <div className="flex justify-center text-center bg-dark w-full py-6 rounded-lg">
+    <div className="flex justify-center text-center bg-gray-900  w-full py-6 rounded-lg">
       {items.map((item, index) => (
         <div key={index} className="flex-1 flex flex-col justify-start items-center">
           {item}
